03c5e1e81d2c086611cb7ff223b7c7daa75dcff1
gustavo.hsr02 <gustavo.hsr02@gmail.com>
2026-03-17 11:14:27 -0300

    Fundamentos Java

---
+++ src/arrays/Exercicio.java
@@ -0,0 +1,13 @@
+package arrays;
+
+public class Exercicio {
+
+	public static void main(String[] args) {
+		// Cria um array de Strings contendo nomes de marcas de carros
+		String[] carros = {"Volvo", "BMW", "Ford", "Mazda"};
+		// Imprime no console o primeiro elemento do array (índice 0)
+		System.out.println(carros [0]);
+
+	}
+
+}
